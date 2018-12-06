b366387430770de4507fcd29e6a579bf95f40ce6
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-12-06 18:23:09 -0500

    Some initial files

---
+++ comp_204/24_lists.py
@@ -0,0 +1,14 @@
+thisList = [1, 4, 1, 2, 3, 10]
+
+a = thisList.reverse()
+b = thisList.sort()
+c = sorted(thisList)
+d = min(thisList)
+e = max(thisList)
+
+print(a)
+print(b)
+print(c)
+print(b is c)
+print(d)
+print(e)
---
+++ comp_204/33_func.py
@@ -0,0 +1,14 @@
+# Function header
+def functionName(argument1, argument2):
+    # body of function
+    pass
+# Rest of program
+
+# Function header
+
+
+def welcomeClass():
+    print("****************")
+    print("Hello, Comp 204!")
+    print("****************")
+# Rest of program
